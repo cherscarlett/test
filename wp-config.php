1197c6f6527eca6fdb188621dfe945f8ed8134a0
Cher Stewart <cher.stewart@wwt.com>
2017-05-02 21:56:02 -0500

    move stuff to environment variables

--- wp-config.php
+++ wp-config.php
@@ -18,22 +18,19 @@
  * @package WordPress
  */
 
+// ** Heroku Postgres settings - from Heroku Environment ** //
+$db = parse_url($_ENV["DATABASE_URL"]);
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'usatDB4oaxi');
-
+define('DB_NAME', trim($db["path"],"/"));
 /** MySQL database username */
-define('DB_USER', 'usatDB4oaxi');
-
+define('DB_USER', $db["user"]);
 /** MySQL database password */
-define('DB_PASSWORD', 'sVKE0ykhSF');
-
+define('DB_PASSWORD', $db["pass"]);
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
-
+define('DB_HOST', $db["host"]);
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
@@ -46,14 +43,16 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'OH*ilK:1pSW;x-H59~hl1#[hGO#ltS18wV:5-SZ9@|kCK[1oOV:s-ZCGsgk0|[VKN');
-define('SECURE_AUTH_KEY',  'Dx5a.h]Oi1WKt#V-S-5dRw9Z!G!Ng[NCr|JsRz}R^F3cvF$Bj,MBj$^E3bDX*X#Le');
-define('LOGGED_IN_KEY',    '<b;uLPEmX6A;XLP<xEH6mae2#PDH*q9;1la_x+SDthlD:WKO_xG5pth[-SWK|lCG1');
-define('NONCE_KEY',        'tDH5hla#]~Dthp1#[hKO#ptoRV:s-R48wVdC|[gF@|kCJ[0vU}4z!Y8v@cBJ>0nNU');
-define('AUTH_SALT',        'ImL.{qPW6t+a2A*aP*#+DL9iWe#;.SaOxlt5];aPS#-~K9Dpeh9_#ZOS~swD15hWa');
-define('SECURE_AUTH_SALT', '6bL.iA{uT{;.X6uyiH#eiW*qHLApei9]SHL.iD15tS];~W5-~t5#dhW~txSGpdh9');
-define('LOGGED_IN_SALT',   'K#-D19pwl:_#SGOwlpC15hVZ|-~K9CwkoG[:kOR[owV4Czc08@|dCK>ovN}4sVZ0z');
-define('NONCE_SALT',       '^YJvjn3>0fUY,y^I7Bncj3<{XMQ$quB3mbj{*.QEI^jnL;2qTXy+I6Euiq2<;XL');
+ define('AUTH_KEY',              getenv('AUTH_KEY'));
+ define('SECURE_AUTH_KEY',       getenv('SECURE_AUTH_KEY'));
+ define('LOGGED_IN_KEY',         getenv('LOGGED_IN_KEY'));
+ define('NONCE_KEY',             getenv('NONCE_KEY'));
+ define('AUTH_SALT',             getenv('AUTH_SALT'));
+ define('SECURE_AUTH_SALT',      getenv('SECURE_AUTH_SALT'));
+ define('LOGGED_IN_SALT',        getenv('LOGGED_IN_SALT'));
+ define('NONCE_SALT',            getenv('NONCE_SALT'));
+ define('AWS_ACCESS_KEY_ID',     getenv('AWS_ACCESS_KEY_ID'));
+ define('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
 
 /**#@-*/
 

022c14bde2a57700dc96d1b37c32bc23be496bc8
Cher Stewart <cher.stewart@wwt.com>
2017-05-02 22:02:51 -0500

    add port

--- wp-config.php
+++ wp-config.php
@@ -28,7 +28,7 @@ define('DB_USER', $db["user"]);
 /** MySQL database password */
 define('DB_PASSWORD', $db["pass"]);
 /** MySQL hostname */
-define('DB_HOST', $db["host"]);
+define('DB_HOST', $db["host"] . ':5432');
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
